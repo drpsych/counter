16e99c29d9ebfca734c72feb4f9c52736b7a2cb2
drpsych <62787554+drpsych@users.noreply.github.com>
2023-03-15 15:08:36 -0400

    code cleanup

--- app.js
+++ app.js
@@ -8,16 +8,18 @@ const btns = document.querySelectorAll('.btn');
 // console.log(btns);
 
 btns.forEach(function (btn) {
-    // console.log(btn);
     btn.addEventListener('click', function (e) {
         const styles = e.currentTarget.classList;
         if (styles.contains("decrease")) {
+            console.log('decrease');
             count--;
         }
         else if (styles.contains("increase")) {
+            console.log('increase');
             count++;
         }
         else {
+            console.log('reset');
             count = 0;
         }
         if (count > 0) {
